4c18f8fc0e79bcd987ce5a1c725d294fb8e3ff85
Naveen <nk1383886@gmail.com>
2016-08-29 15:13:54 +0530

    all files added

---
+++ app/controllers/abc_controller.rb
@@ -0,0 +1,7 @@
+class AbcController < ApplicationController
+ acts_as_token_authentication_handler_for User
+
+  def asdf
+    render :json => { :message => 'Well done!' }
+  end
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::API
+include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+
+  respond_to :json
+
+end
